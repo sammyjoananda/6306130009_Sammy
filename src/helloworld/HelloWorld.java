f95e7a9f460e3d1a47a1c37cf1ec1fe677b6cee0
sammyjoananda <sammyjoananda@gmail.com>
2015-04-07 07:38:13 +0700

    hello_6306130009
    tugas RPL MOD8

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author sammyjoananda
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hello,Saya Sammy Jo Ananda. Nim saya 6306130009. saya sangat menyukai praktikum RPL Modul 8 ini.");
+	}
+    }
+    
+
